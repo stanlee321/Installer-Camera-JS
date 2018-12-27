844b5b01d7e4706b7172a09136713ceeaa95f6f6
stanlee321 <stanlee321@gmail.com>
2018-12-27 01:08:08 -0400

    updated main

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const image_id = document.getElementById('imageID')
 const x_text = document.getElementById("x_text")
 const y_text = document.getElementById("y_text")
@@ -61,9 +60,6 @@ class Installer {
         this.GetCoordinates()
     }
     
-    showList(){
-       list.children[0].innerHTML += "<li>"+names[names.length-1]+"</li>"; 
-    }
     GetCoordinates(){
         $(document).ready(function() {
             $("#imageID").on("click", function(event) {
@@ -89,48 +85,3 @@ class Installer {
 function startInstaller() {
     window.install = new Installer()
 }
-
-
-/*
-function FindPosition(oElement)
-{
-  if(typeof( oElement.offsetParent ) != "undefined")
-  {
-    for(var posX = 0, posY = 0; oElement; oElement = oElement.offsetParent)
-    {
-      posX += oElement.offsetLeft;
-      posY += oElement.offsetTop;
-    }
-      return [ posX, posY ];
-    }
-    else
-    {
-      return [ oElement.x, oElement.y ];
-    }
-}
-
-function GetCoordinates(e)
-{
-  var PosX = 0;
-  var PosY = 0;
-  var ImgPos;
-  ImgPos = FindPosition(myImg);
-  if (!e) var e = window.event;
-  if (e.pageX || e.pageY)
-  {
-    PosX = e.pageX;
-    PosY = e.pageY;
-  }
-  else if (e.clientX || e.clientY)
-    {
-      PosX = e.clientX + document.body.scrollLeft
-        + document.documentElement.scrollLeft;
-      PosY = e.clientY + document.body.scrollTop
-        + document.documentElement.scrollTop;
-    }
-  PosX = PosX - ImgPos[0];
-  PosY = PosY - ImgPos[1];
-  document.getElementById("x").innerHTML = PosX;
-  document.getElementById("y").innerHTML = PosY;
-}
-*/
